9f37a1d3c1a019980cc3136001455eac96b8b51a
RippedHealer <bypassforrh@protonmail.com>
2024-06-25 12:41:33 +0530

    Inital Commit

---
+++ src/main.cpp
@@ -0,0 +1,25 @@
+#include <cstdlib>
+#include <dpp/dpp.h>
+
+std::string BOT_TOKEN;
+
+int main() {
+	BOT_TOKEN = std::string(std::getenv("BOT_TOKEN"));
+	dpp::cluster bot(BOT_TOKEN);
+
+	bot.on_log(dpp::utility::cout_logger());
+
+	bot.on_slashcommand([](const dpp::slashcommand_t& event) {
+			if (event.command.get_command_name() == "ping") {
+			event.reply("Pong!");
+			}
+			});
+
+	bot.on_ready([&bot](const dpp::ready_t& event) {
+			if (dpp::run_once<struct register_bot_commands>()) {
+			bot.global_command_create(dpp::slashcommand("ping", "Ping pong!", bot.me.id));
+			}
+			});
+
+	bot.start(dpp::st_wait);
+}
